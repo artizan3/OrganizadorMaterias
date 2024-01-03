571e40898a0de8aba52e1fc2ef91638f1d8d6536
artizan3 <129905900+artizan3@users.noreply.github.com>
2024-01-03 18:10:20 -0300

    avanze

--- organizador.js
+++ organizador.js
@@ -1,16 +1,67 @@
-let contador = 1;
+let cont=1;
+function crearDiv(){
+  var myDiv = document.createElement("div");
+  myDiv.id = "Materias" + cont;
+  myDiv.className = "Materia";
 
-function agregarDiv() {
-  // Crear un nuevo div
-  const nuevoDiv = document.createElement('div');
-  nuevoDiv.className = 'nuevo-div';
-  nuevoDiv.textContent = 'Div ' + contador;
+  // Crear el encabezado del contenedor
+  var myDivHeader = document.createElement("div");
+  myDivHeader.id = "Materiasheader" + cont;
+  myDivHeader.className = "MateriaHead";
+  myDivHeader.innerHTML = "Titulo materia";
 
+  // Crear el párrafo dentro del contenedor
+  var paragraph = document.createElement("p");
+  paragraph.innerHTML = "datos materia";
 
-  // Agregar el nuevo div al contenedor
-  const contenedor = document.getElementById('contenedor');
-  contenedor.appendChild(nuevoDiv);
+  // Agregar el encabezado y el párrafo al contenedor principal
+  myDiv.appendChild(myDivHeader);
+  myDiv.appendChild(paragraph);
 
-  // Incrementar el contador
-  contador++;
+  document.body.appendChild(myDiv);
+
+  dragElement(myDiv);
+  cont++;
+
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+       /* if present, the header is where you move the DIV from:*/
+      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+      /* otherwise, move the DIV from anywhere inside the DIV:*/
+      elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
